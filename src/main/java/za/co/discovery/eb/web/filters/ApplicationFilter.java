20a8c52cbf3325fbd60e9533b49c82fbb5279448
lindikha1 <lindikhayam@discovery.co.za>
2018-06-07 08:56:37 +0200

    Show cookies

--- src/main/java/za/co/discovery/eb/web/filters/ApplicationFilter.java
+++ src/main/java/za/co/discovery/eb/web/filters/ApplicationFilter.java
@@ -18,6 +18,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class ApplicationFilter implements Filter {
@@ -27,6 +28,7 @@ public class ApplicationFilter implements Filter {
 
   public void doFilter(ServletRequest req, ServletResponse resp,
                        FilterChain chain) throws IOException, ServletException {
+    showCookies(req);
     String path = ((HttpServletRequest) req).getRequestURI();
     if (path.contains("/assets") || path.contains(".js") || path.contains(".css") || path.contains(".png") || path
         .contains(".svg")) {
@@ -35,6 +37,19 @@ public class ApplicationFilter implements Filter {
     else {
       RequestDispatcher rd = req.getRequestDispatcher("/index.html");
       rd.forward(req, resp);
+    }
+  }
+
+  private void showCookies(ServletRequest req) {
+    try{
+      HttpServletRequest request = (HttpServletRequest) req;
+      Cookie[] cookies = request.getCookies();
+
+      for (Cookie ck : cookies) {
+        System.out.println(ck.getDomain()+" "+ck.getName()+" "+ck.getSecure()+" "+ck.getPath()+" "+ck.getValue());
+      }
+    }catch(Exception ex){
+      ex.printStackTrace();
     }
   }
 
